3eed75b2fed30c9f0ce173120470d5bb84693ef1
Agenciatektus <agenciatektus@gmail.com>
2025-05-27 09:45:41 -0300

    fix: atualiza configuração de imagens para incluir todos os domínios necessários

--- next.config.js
+++ next.config.js
@@ -8,6 +8,26 @@ const nextConfig = {
         hostname: 'randomuser.me',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      }
     ],
   },
 }
